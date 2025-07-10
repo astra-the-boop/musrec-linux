b2d19a4961a8db491d1c9fd7de75a30ba4e6ef59
astra-the-boop <astra-the-boop@icloud.com>
2025-07-10 15:36:05 +0700

    working on tracks.py, recoding some shit for linux because the macos code was really applescript reliant. luckily there's this library called dbus or something :P

--- tracks.py
+++ tracks.py
@@ -1,4 +1,7 @@
 import requests
+import subprocess
+import dbus
+import re
 
 def fetchAlbumCover(title, artist, album, saveAs="cover.jpg"):
     query = f"{title} {artist} {album}".replace(" ","+")
@@ -13,3 +16,23 @@ def fetchAlbumCover(title, artist, album, saveAs="cover.jpg"):
         f.write(imgData)
     return saveAs
 
+def getMprisPlayer(service="spotify"):
+    bus = dbus.SessionBus()
+    for services in bus.list_names():
+        if service.lower() in services.lower() and services.startswith("org.mpris.MediaPlayer2."):
+            return bus.get_object(services, "/org/mpris/MediaPlayer2")
+        raise RuntimeError(f"No MPRIS player found for {service}")
+
+def getMetadata(player):
+    interface = dbus.Interface(player,dbus_interface="org.freedesktop.DBus.Properties")
+    metadata = interface.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    return metadata
+
+def getDuration(service="spotify"):
+    player = getMprisPlayer(service)
+    metadata = getMetadata(player)
+    return int(metadata.get("mpris:length", 0)) / 1000000 #for some stupid ass reason, shit's in microseconds, yes. microseconds. not ms, micro.
+                                                          #yk what else is micro? your d- unfunny sorry not sorry
+
+def getPosition(service="spotify"):
+    player = getMprisPlayer()
